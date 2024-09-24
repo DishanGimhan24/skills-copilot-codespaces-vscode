82705a39ff192887cf2487aa1e2141c7df658718
Dishan Gimhan <it22108340@my.sliit.lk>
2024-09-24 08:45:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("contact").style.display = "none";   
+}
